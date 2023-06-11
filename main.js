3ba0bc1fc6a369ba077b7397d339259239a23fbd
Itay Fintz <itayfi@gmail.com>
2023-06-11 22:06:41 +0300

    Use face detection and GrabCut

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import './style.css'
 
 let videoEl, cap, bgImg;
-let fgbg = new cv.BackgroundSubtractorMOG2(500, 16, true);
-let frameInput, frameOutput, fgmask, fgmask2;
-// let fgmask;
+let frameInput, frameInputRGB, frameOutput, frameGray, fgmask;
+let classifier = new cv.CascadeClassifier();
+let faces = new cv.RectVector();
+let bgdModel = new cv.Mat();
+let fgdModel = new cv.Mat();
 
 async function onLoad() {
   // Get camera stream
@@ -11,6 +13,15 @@ async function onLoad() {
   videoEl = document.createElement('video');
   videoEl.srcObject = stream;
   bgImg = document.getElementById('bgImg');
+  
+  document.getElementById('bg-select').addEventListener('change', (e) => {
+    bgImg.src = URL.createObjectURL(e.target.files[0]);
+  });
+  
+  const faceRecResp = await fetch('haarcascade_frontalface_default.xml');
+  const faceRecData = new Uint8Array(await faceRecResp.arrayBuffer());
+  cv.FS_createDataFile('/', 'haarcascade_frontalface_default.xml', faceRecData, true, false, false);
+  classifier.load('haarcascade_frontalface_default.xml');
 
   videoEl.addEventListener('loadedmetadata', () => {
     const canvas = document.getElementById('canvasOutput');
@@ -23,47 +34,59 @@ async function onLoad() {
     bgImg.height = videoEl.videoHeight;
     bgImg.style.display = 'block';
 
-    // fgmask = new cv.Mat(videoEl.height, videoEl.width, cv.CV_8UC1);
     frameInput = new cv.Mat(videoEl.height, videoEl.width, cv.CV_8UC4);
     frameOutput = new cv.Mat(videoEl.height, videoEl.width, cv.CV_8UC4);
+    frameInputRGB = new cv.Mat(videoEl.height, videoEl.width, cv.CV_8UC3);
+    frameGray = new cv.Mat();
     fgmask = new cv.Mat(videoEl.height, videoEl.width, cv.CV_8UC1);
-    fgmask2 = new cv.Mat(videoEl.height, videoEl.width, cv.CV_8UC1);
 
     cap = new cv.VideoCapture(videoEl);
     animate();
   });
 }
 
-function animate() {
-  try {
-    cap.read(frameInput);
-    frameOutput.setTo([0, 0, 0, 0]);
 
-    fgbg.apply(frameInput, fgmask);
-    // const ksize = new cv.Size(3, 3);
-    // cv.GaussianBlur(fgmask, fgmask2, ksize, 0, 0, cv.BORDER_DEFAULT);
+let lastDt = 0;
+function animate(dt) {
+  let fps = 1000 / (dt - lastDt);
+  document.getElementById('fps').innerText = fps.toFixed(1);
+  cap.read(frameInput);
+  cv.cvtColor(frameInput, frameInputRGB, cv.COLOR_RGBA2RGB, 0);
+  frameOutput.setTo([0, 0, 0, 0]);
 
-    cv.add(frameOutput, frameInput, frameOutput, fgmask);
-    // cv.mixChannels([frameInput, fgmask2], [frameOutput], [0,0, 1,1, 2,2, 4,3]);
+  cv.cvtColor(frameInput, frameGray, cv.COLOR_RGBA2GRAY, 0);
 
-    // cv.imshow('canvasOutput', frameOutput);
-    cv.imshow('canvasOutput', frameOutput);
-    requestAnimationFrame(animate);
-  } catch (e) {
-    console.error(e);
-    document.getElementById('app').innerHTML = `
-      <h1 class="error">An error occurred</h1>
-      <p>${e}</p>
-      <p>Try reloading the page.</p>
-    `;
-    bgImg.style.display = 'none';
-    document.getElementById('canvasOutput').style.display = 'none';
-  } finally {
-    // frameInput.delete();
-    // frameOutput.delete();
-    // fgmask.delete();
-    // fgmask2.delete();
+  classifier.detectMultiScale(frameGray, faces, 1.1, 3, 0);
+
+  fgmask.setTo([cv.GC_PR_BGD, cv.GC_PR_BGD, cv.GC_PR_BGD, cv.GC_PR_BGD]);
+  for (let i = 0; i < faces.size(); ++i) {
+    let face = faces.get(i);
+    let point1 = new cv.Point(face.x, face.y);
+    let point2 = new cv.Point(face.x + face.width, face.y + face.height);
+    cv.rectangle(fgmask, point1, point2, [cv.GC_PR_FGD, cv.GC_PR_FGD, cv.GC_PR_FGD, cv.GC_PR_FGD], cv.FILLED);
+    let point3 = new cv.Point(face.x, face.y + face.height);
+    let point4 = new cv.Point(face.x + face.width, face.y + 2 * face.height);
+    cv.rectangle(fgmask, point3, point4, [cv.GC_PR_FGD, cv.GC_PR_FGD, cv.GC_PR_FGD, cv.GC_PR_FGD], cv.FILLED);
   }
+  if (faces.size() > 0) {
+    cv.grabCut(frameInputRGB, fgmask, faces.get(0), bgdModel, fgdModel, 1, cv.GC_INIT_WITH_MASK);
+  }
+
+  for (let x = 0; x < frameInputRGB.cols; x++) {
+    for (let y = 0; y < frameInputRGB.rows; y++) {
+      let pixel = fgmask.ucharPtr(y, x);
+      if (pixel[0] == cv.GC_BGD || pixel[0] == cv.GC_PR_BGD) {
+        pixel[0] = 0;
+        pixel[1] = 0;
+        pixel[2] = 0;
+      }
+    }
+  }
+
+  cv.add(frameOutput, frameInput, frameOutput, fgmask);
+
+  cv.imshow('canvasOutput', frameOutput);
+  requestAnimationFrame(animate);
 }
 
 document.addEventListener('DOMContentLoaded', onLoad);
